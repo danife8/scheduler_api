52a06c3047f8347bedce9917c0449e8bcf1b76b2
Daniel Fe <daniel.fe.92@gmail.com>
2024-06-10 03:09:25 -0300

    added specialists tests and seeds

--- db/seeds.rb
+++ db/seeds.rb
@@ -3,6 +3,9 @@ require 'factory_bot_rails'
 5.times do
   FactoryBot.create(:patient)
 end
-
 puts "#{Patient.all.count} patients created"
 
+5.times do
+  FactoryBot.create(:specialist)
+end
+puts "#{Specialistt.all.count} specialists created"
